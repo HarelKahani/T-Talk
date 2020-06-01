6e4f252c853c21b252dce2e589f39f942ff55ba1
arbelhizmi <43061885+arbelhizmi@users.noreply.github.com>
2020-06-01 19:32:04 +0300

    after crysis

--- react_demo_shai/src/App.js
+++ react_demo_shai/src/App.js
@@ -13,13 +13,10 @@ import NotFoundPage from './pages/404'
 import TherapistMenu from './pages/TherapistMenu'
 import ChooseTopic from './pages/ChooseTopic'
 import ManageTopic from './pages/ManageTopic';
-<<<<<<< HEAD
-import Board from './Board/Board';
-=======
 import ManageSupriseCard from './pages/ManageSupriseCard';
+import Board from './Board/Board'
+import { Button } from 'react-bootstrap';
 
-
->>>>>>> 5c1e8b37e3cf78aa0cdc1a4c945671307b826209
 
 class App extends Component {
   render() {
@@ -33,12 +30,8 @@ class App extends Component {
             <TherapistMenu />
             <Route path="/ChooseTopic" component={ChooseTopic} />
             <Route path="/ManageTopic" component={ManageTopic} />
-<<<<<<< HEAD
-            <Route path="/Board" component={Board} />
-=======
             <Route path="/ManageSupriseCard" component={ManageSupriseCard} />
-            
->>>>>>> 5c1e8b37e3cf78aa0cdc1a4c945671307b826209
+            <Route path="/Board" component={Board} />
           </div>
           <Route path="/404" component={NotFoundPage} />
           <Redirect to="/404" />
