c5aad986dd0bf51b3faa961e1e05d3b940fdb17e
Harel Kahani <proj.t.talk@gmail.com>
2020-06-04 16:04:24 +0300

    finishing card pack visualization

--- react_demo_shai/src/Board/CardsModal.js
+++ react_demo_shai/src/Board/CardsModal.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-
+//define the amount of cards in one pack rotine
+const CARDS_LIMIT=16;
 
 export class CardsModal extends Component {
     constructor(props) {
         super(props);
-        //this.getSubjectName = this.getSubjectName.bind(this);
-        //this.addSubject = this.addSubject.bind(this);
-        this.state = { counter: 0 }
+        this.state = { counter: 1 }
     }
+    onClick = () => {
+        this.props.onHide();
+        this.setState( {counter: this.state.counter += 1});
+        if(this.state.counter > CARDS_LIMIT){
+            this.setState( {counter: 0});
+        }
+    }
+
     render() {
 
         return (
             <div>
-                
-                    <Modal
-                        {...this.props}
-                        size="xl"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                    //scrollable
-                    >
-                        <Modal.Header>
-                            <Modal.Title id="contained-modal-title-vcenter">
-                                <b>משימה</b>
-                            </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                           <img src="/cards_imgs/1.JPG"></img>
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button type="submit" onClick={this.props.onHide} style={{ width: "20%" }}>המשך</Button>
-                        </Modal.Footer>
-                    </Modal>
+                <Modal
+                    {...this.props}
+                    size="xl"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                //scrollable
+                >
+                    <Modal.Header>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            <b>משימה</b>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img src={`/cards_imgs/${this.state.counter}.JPG`}></img>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button type="submit" onClick={this.onClick} style={{ width: "20%" }}>המשך</Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         );
     }
