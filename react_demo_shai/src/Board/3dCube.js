e34286ba90b2f20da9126f1d7e0904835531cc9f
Harel Kahani <proj.t.talk@gmail.com>
2020-06-04 18:56:42 +0300

    changing cube visuaization

--- react_demo_shai/src/Board/3dCube.js
+++ react_demo_shai/src/Board/3dCube.js
@@ -27,7 +27,7 @@ export class Cube extends Component {
 
     render() {
         return (
-            
+          
           <div className="cube_inner_container">
              
             <div className="dice" onClick={rollDice}>
@@ -39,8 +39,8 @@ export class Cube extends Component {
                 <li className="die-item" data-side="5"></li>
                 <li className="die-item" data-side="6"></li>
               </ol>
+              <h4 className="cube_instru" style={{textAlign:"center"}}>לחץ על הקובייה להטלה</h4>
             </div>
-            <button type="button" id="roll-button" onClick={rollDice} >הטל קובייה</button>
             </div>
         );
     }
