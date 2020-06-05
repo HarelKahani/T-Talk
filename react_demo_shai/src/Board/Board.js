b6c1bdaadbb63e9ef9cad480732c2b1a94997ec7
arbelhizmi <43061885+arbelhizmi@users.noreply.github.com>
2020-06-05 14:45:17 +0300

    enable-disable buttons

--- react_demo_shai/src/Board/Board.js
+++ react_demo_shai/src/Board/Board.js
@@ -11,6 +11,32 @@ class Board extends React.Component {
         document.getElementById(`${id}`).innerHTML = `${id}`;
     }
 
+    fillArray = () => {
+        let buttonArray = [];
+        let idIndex;
+        for (idIndex = 1; idIndex < 29; idIndex++) {
+            buttonArray[idIndex] = document.getElementById(`button${idIndex}`);
+        }
+        return buttonArray;
+    }
+
+    enableDisable = (id) => {
+        let buttonArray = this.fillArray();
+        console.log(buttonArray);
+        if (id === `enable`) {
+            console.log(`enable was pressed`);
+            buttonArray.map((button) => {
+                button.disabled = false;
+            });
+        } 
+        if (id === `disable`) {
+            console.log(`disable was pressed`);
+            buttonArray.map((button) => {
+                button.disabled = true;
+            });
+        }
+    }
+
 
     render() {
         return (
@@ -23,6 +49,15 @@ class Board extends React.Component {
 
                 <div className="cube_container">
                     <Cube />
+                </div>
+
+                <div id="enbale-disable">
+                    <Button id="disable" onClick={e => this.enableDisable(e.target.id)}>
+                        Disable Board
+                    </Button>
+                    <Button id="enable" onClick={e => this.enableDisable(e.target.id)}>
+                        Enable Board
+                    </Button>
                 </div>
                 
                 <div id="path_container">
@@ -37,7 +72,7 @@ class Board extends React.Component {
                         <Button id="button28" style={{ backgroundColor: 'green' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button27" style={{ backgroundColor: 'blue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button26" style={{ backgroundColor: 'pink' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
-                        <Button id="button25" style={{ backgroundColor: 'cyan' ,borderRadius: "20px 0px 0px 0px" }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
+                        <Button id="button25" style={{ backgroundColor: 'lightblue' ,borderRadius: "20px 0px 0px 0px" }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <br />
                         <Button disabled></Button>
                         <Button disabled></Button>
@@ -52,7 +87,7 @@ class Board extends React.Component {
                         <br />
                         <Button id="button13" style={{ backgroundColor: 'pink' ,borderRadius: "0px 20px 0px 0px" }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button14" style={{ backgroundColor: 'green' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
-                        <Button id="button15" style={{ backgroundColor: 'cyan' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
+                        <Button id="button15" style={{ backgroundColor: 'lightblue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button16" style={{ backgroundColor: 'blue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button17" style={{ backgroundColor: 'pink' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button18" style={{ backgroundColor: 'yellow' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
@@ -75,12 +110,12 @@ class Board extends React.Component {
                         <Button disabled></Button>
                         <br />
                         <Button id="button11" style={{ backgroundColor: 'yellow' ,borderRadius: "0px 0px 20px 0px" }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
-                        <Button id="button10" style={{ backgroundColor: 'cyan' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
+                        <Button id="button10" style={{ backgroundColor: 'lightblue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button9" style={{ backgroundColor: 'pink' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button8" style={{ backgroundColor: 'blue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button7" style={{ backgroundColor: 'green' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button6" style={{ backgroundColor: 'yellow' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
-                        <Button id="button5" style={{ backgroundColor: 'cyan' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
+                        <Button id="button5" style={{ backgroundColor: 'lightblue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button4" style={{ backgroundColor: 'pink' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button3" style={{ backgroundColor: 'blue' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
                         <Button id="button2" style={{ backgroundColor: 'green' }} onClick={e => this.handleClick(e.target.id, e.target.style.backgroundColor)}></Button>
