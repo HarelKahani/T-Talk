300631f4aee519e3c85be2a5bb893a11f4d7923c
Shaike77 <shaike77@gmail.com>
2020-06-05 06:59:09 +0300

    choose topic now connected to storage

--- react_demo_shai/src/pages/ChooseTopic.js
+++ react_demo_shai/src/pages/ChooseTopic.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import {storage} from '../pages/HomePage'
 
 
 class ChooseTopic extends Component {
+    constructor(props) {
+        super(props);
+        this.getSubjectName = this.getSubjectName.bind(this);
+        this.getAllSubjectNames = this.getAllSubjectNames.bind(this);
+        this.state = {  
+            SubjectNameval: "" ,
+            SubjectName: [], 
+            addModalShowForSubjUpload: false 
+        }
+        this.getAllSubjectNames();
+    }
+
+    getAllSubjectNames = () => {
+        let topics = storage.ref('topics/')
+        console.log(topics)
+        return topics.listAll().then(event =>{
+            let list = event.prefixes
+            console.log(list);
+            list.map((item, index) => {
+                this.setState({
+                    SubjectName: this.state.SubjectName.concat(item.name)
+                })
+            })
+        }).catch(err => {
+            console.log(err)
+        });  
+    }
+
+    getSubjectName = event => {
+        this.setState({ SubjectNameval: event.target.value });
+    };
+    
+    showRow = (i) => {                       
+        let topics = this.state.SubjectName
+        return topics.map((item, index)=>{
+            return <tr>
+                <td>{index+1}</td>
+                <td > {item} </td>
+                <td> <Button variant="outline-primary" style={{ width: "20%"}}>התחל משחק</Button></td>
+            </tr>
+        })
+    }
+
+    showAllRows = () => {
+        let all = new Object;
+        console.log("here")
+        for (let i=0; i<this.state.SubjectName.length; i++){
+            all.insert(this.showRow(i))
+        }
+        return all
+    }
+    
 
     render() {
         return (
-
-
+            <div>
             <Table striped bordered hover>
-
                 <thead>
                     <tr>
                         <th >#</th>
@@ -18,24 +69,44 @@ class ChooseTopic extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td > הגייה של האות צ</td>
-                        <td> <Button variant="outline-primary"style={{ width: "20%"}}>התחל משחק</Button></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>הגייה של האות ש</td>
-                        <td> <Button variant="outline-primary" style={{ width: "20%"}}>התחל משחק</Button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td > הגייה של האות ר</td>
-                        <td> <Button variant="outline-primary" style={{ width: "20%"}}>התחל משחק</Button></td>
-                    </tr>
+                    {this.showRow()}
                 </tbody>
-
             </Table>
+        </div>
+
+
+
+
+
+
+            // <Table striped bordered hover>
+
+            //     <thead>
+            //         <tr>
+            //             <th >#</th>
+            //             <th>שם הנושא</th>
+            //             <th>בחר נושא</th>
+            //         </tr>
+            //     </thead>
+            //     <tbody>
+            //         <tr>
+            //             <td>1</td>
+            //             <td > הגייה של האות צ</td>
+            //             <td> <Button variant="outline-primary"style={{ width: "20%"}}>התחל משחק</Button></td>
+            //         </tr>
+            //         <tr>
+            //             <td>2</td>
+            //             <td>הגייה של האות ש</td>
+            //             <td> <Button variant="outline-primary" style={{ width: "20%"}}>התחל משחק</Button></td>
+            //         </tr>
+            //         <tr>
+            //             <td>3</td>
+            //             <td > הגייה של האות ר</td>
+            //             <td> <Button variant="outline-primary" style={{ width: "20%"}}>התחל משחק</Button></td>
+            //         </tr>
+            //     </tbody>
+
+            // </Table>
 
 
 
